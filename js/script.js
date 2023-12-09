11c5881888f341c3cd4a5c9c401218eb90c2b2b7
murtaki shihab <murtakibangko@gmail.com>
2023-12-10 01:56:34 +0700

    menu tambahan

--- js/script.js
+++ js/script.js
@@ -4,11 +4,30 @@ const NavbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick= ()=>{
     NavbarNav.classList.toggle('active');
 }
+// Searching
+const searchForm =document.querySelector('.search-form');
+const searchBox  =document.querySelector('#search-box');
+document.querySelector('#search-button').onclick=(e)=>{
+    e.preventDefault();
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+}
+
 
 // Klik semberangan
-const humberger= document.querySelector('#hamburger-menu');
+const hm =document.querySelector('#hamburger-menu');
+const sb =document.querySelector('#search-button');
+
 document.addEventListener('click', function(e){
-    if(!humberger.contains(e.target) && !NavbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !NavbarNav.contains(e.target)){
         NavbarNav.classList.remove('active');
     }
+
+    if(!sb.contains(e.target) && !NavbarNav.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
 });
+
+
+
+
